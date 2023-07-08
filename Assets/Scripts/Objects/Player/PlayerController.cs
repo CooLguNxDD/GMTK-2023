cf0cf294acfb32e37d5e0c280e60226d5659ddde
davidjia223 <davidjia223@gmail.com>
2023-07-07 19:21:05 -0700

    updated player animation

--- Assets/Scripts/Objects/Player/PlayerController.cs
+++ Assets/Scripts/Objects/Player/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private UnitSetting unitSetting;
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
 
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -42,24 +43,22 @@ public class PlayerController : MonoBehaviour
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         isWalking = moveDir != Vector3.zero;
+        animator.SetBool("IsWalking", isWalking);
+        
 
         // float rotationSpeed = 10f;
         // transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 
     private void RotateInDirectionOfInput(){
-    if (_movementInput != Vector2.zero){
-        float targetAngle = Mathf.Atan2(_smoothedMovementInput.y, _smoothedMovementInput.x) * Mathf.Rad2Deg;
-        Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        if (_movementInput != Vector2.zero){
+            float targetAngle = Mathf.Atan2(_smoothedMovementInput.y, _smoothedMovementInput.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, 0, targetAngle);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        }
     }
-}
-
 
     public bool IsWalking(){
         return isWalking;
     }
-
-
-
 }
